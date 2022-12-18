c2c00e850f9bf6963a977715ed180321d3ab46d2
Ltw32 <lthomaswebb@gmail.com>
2022-12-17 19:37:44 -0700

    hopefully fixed the 64 emulator
    yey

--- N64Wasm-master/dist/romlist.js
+++ N64Wasm-master/dist/romlist.js
@@ -1,6 +1,9 @@
 var ROMLIST = [
 
     {url:"roms/oot.z64",title:"Ocarina of time"},
-
+    {url:"roms/mariokart64.n64",title:"Mariokart 64"},
+    {url:"roms/kirby64.z64",title:"Kirby 64"},
+    {url:"roms/sm64.z64",title:"Super Mario 64"},
+    {url:"roms/ssb64.z64",title:"Super Smash Bros"},
 
 ];
